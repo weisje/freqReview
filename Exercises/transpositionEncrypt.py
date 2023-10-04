7c0641feae6827c909b752265d3b02c955a008b9
weisje <20269574+weisje@users.noreply.github.com>
2023-10-04 07:06:41 -0400

    -Created TranspositionEncrypt branch off of main branch, -Generated Exercises > transpositionEncrypt.py file.  Established main() function. Established encryptMessage() function which encrypts the message via a transposition cipher

---
+++ Exercises/transpositionEncrypt.py
@@ -0,0 +1,23 @@
+# Transposition Cipher Encryption
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+def main():
+    pass
+
+
+def encryptMessage(key, message):
+    """
+    Function for encrypting provided message as transposition cipher
+    :param key: length of each line for the provided message
+    :type key: int
+    :param message: Message to be encrypted with transposition cipher
+    :type message: str
+    :return: str
+    """
+
+    cipherText = [''] * key
+
+    return ''.join(cipherText)
+
+if __name__ == '__main__':
+    main()
