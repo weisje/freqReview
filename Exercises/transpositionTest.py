89af1836ca5a808c1b795ad34aac355f6db04782
weisje <20269574+weisje@users.noreply.github.com>
2023-10-07 07:12:27 -0400

    -Generated TranspositionTest branch off of main branchCreated Exercises > transpositionTest.py with main() function & main running stipulation.

---
+++ Exercises/transpositionTest.py
@@ -0,0 +1,15 @@
+# Transposition Cipher Test
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import random
+import sys
+import transpositionDecrypt
+import transpositionEncrypt
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
