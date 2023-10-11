4176e12c723a36c30991b5a315813535d2c694f6
weisje <20269574+weisje@users.noreply.github.com>
2023-10-11 07:32:29 -0400

    Added check to see if the output file already exists.  If it does it offers the user the ability to exit out before overwriting the existing file.

--- Exercises/transpositionFileCipher.py
+++ Exercises/transpositionFileCipher.py
@@ -12,13 +12,19 @@ def main():
     inputFileName = "frankenstein"
     inputFileType = ".txt"
     fullInputFileName = inputDirectoryName + "\\" + inputFileName + inputFileType
-    outputFileName = inputDirectoryName + inputFileName + ".encrypted" + inputFileType
+    outputFileName = inputDirectoryName + "\\" + inputFileName + ".encrypted" + inputFileType
     cryptKey = 10 # Key to define how long the rows are for this round of crypting
     cryptMode = 'encrypt' # Set to "encrypt" or "decrypt"
 
-    if cryptMode.lower() != 'encrypt' and cryptMode.lower() != 'decrypt':
+    if cryptMode.lower() != 'encrypt' and cryptMode.lower() != 'decrypt': # Check to see if user entered a proper mode setting
         print(f"\'{cryptMode}\' is not a valid mode for this program.  The available modes are \'encrypt\' or \'decrypt\'.  Quitting...")
         sys.exit()
+
+    if os.path.exists(outputFileName):
+        print(f"This will overwrite the file: {outputFileName}. (C)ontinue or (Q)uit?")
+        response = input("> ")
+        if not response.lower().startswith("c"):
+            sys.exit()
 
 if __name__ == '__main__':
     main()
