23e6ea5398b3d78932c8f020a4392d49fe7b47fb
weisje <20269574+weisje@users.noreply.github.com>
2023-10-12 07:05:52 -0400

    Added execution time measuring section, Added script calls for ciphering the provided message based on the cryptMode variable(encrypt on 'encrypt', decrypt on 'decrypt'), & Opened text file to write results of crypting of file.

--- Exercises/transpositionFileCipher.py
+++ Exercises/transpositionFileCipher.py
@@ -13,10 +13,11 @@ def main():
     inputFileType = ".txt"
     cryptKey = 10  # Key to define how long the rows are for this round of crypting
     cryptMode = 'encrypt'  # Set to "encrypt" or "decrypt"
+    cryptMode = cryptMode.lower()
     fullInputFileName = f"{inputDirectoryName}\\{inputFileName}{inputFileType}"
     outputFileName = f"{inputDirectoryName}\\{inputFileName}.encrypted{inputFileType}"
 
-    if cryptMode.lower() != 'encrypt' and cryptMode.lower() != 'decrypt': # Check to see if user entered a proper mode setting
+    if cryptMode != 'encrypt' and cryptMode != 'decrypt': # Check to see if user entered a proper mode setting
         print(f"\'{cryptMode}\' is not a valid mode for this program.  The available modes are \'encrypt\' or \'decrypt\'.  Quitting...")
         sys.exit()
 
@@ -35,6 +36,19 @@ def main():
     fileObj.close()
 
     print(f"{cryptMode.title()}ing...")
+
+    startTime = time.time()
+    if cryptMode == 'encrypt':
+        translated = transpositionEncrypt.encryptMessage(cryptKey, content)
+    elif cryptMode == 'decrypt':
+        translated = transpositionDecrypt.decryptMessage(cryptKey, content)
+    totalTime = round(time.time() - startTime, 2)
+    print(f"{cryptMode.title()}ing time: {totalTime} seconds.")
+
+    outputFileObj = open(outputFileName, 'w')
+    outputFileObj.write(translated)
+    outputFileObj.close()
+
 
 
 if __name__ == '__main__':
