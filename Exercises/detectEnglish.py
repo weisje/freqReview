0e327946379b467c40012569800bcda3c9d3c8f0
weisje <20269574+weisje@users.noreply.github.com>
2023-10-14 06:11:42 -0400

    -Added user instruction comment to detectEnglish.py on how to use the module -Defined the letter cases via string library -Entered function frames for parts of detectEnglish.py     -Added TODO flags for each of the incomplete functions

--- Exercises/detectEnglish.py
+++ Exercises/detectEnglish.py
@@ -1,2 +1,62 @@
 # Detect English Module
 # https://www.nostarch.com/crackingcodes (BSD Licensed)
+
+"""
+To use this code type:
+  import detectEnglish
+  detectEnglish.isEnglish(someString) # Returns True or False
+(There must be a "dictionary.txt" file in this directory with all
+English words in it, one per line.  You can download this from
+https://www.nostarch.com/crackingcodes/.)
+"""
+
+import string
+
+UPPERLETTERS = string.ascii_uppercase
+LETTERS_AND_SPACE = UPPERLETTERS + string.ascii_lowercase + ' \t\n'
+
+
+# TODO
+def loadDictionary() -> dict:
+    """
+
+    :return: dict
+    """
+    pass
+
+
+# TODO
+def getEnglishCount(message) -> float:
+    """
+
+    :param message:
+    :type message:
+    :return: float
+    """
+    pass
+
+
+# TODO
+def removeNonLetters(message) -> str:
+    """
+
+    :param message:
+    :type message:
+    :return: str
+    """
+    pass
+
+
+# TODO
+def isEnglish(message, wordPercentage=20, letterPercentage=85) -> bool:
+    """
+
+    :param message:
+    :type message:
+    :param wordPercentage:
+    :type wordPercentage:
+    :param letterPercentage:
+    :type letterPercentage:
+    :return: bool
+    """
+    pass
