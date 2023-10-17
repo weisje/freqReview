e76bc94efb12bd5ae39f5b0fc9edbcc010464456
weisje <20269574+weisje@users.noreply.github.com>
2023-10-17 07:27:47 -0400

    -Tested program functionality     -Copied sample message from website & pasted it into myMessage     -Result: "key 6: Augusta Ada King-Noel, Countess of Lovelace (10 December 1815 - 27 November 1852) was an English mat"     -Tried again, not stopping it when it came across key 6     -Result: "Trying key: 400...                 Trying key: 410...                 Failed to hack message"

--- Exercises/transpositionHacker.py
+++ Exercises/transpositionHacker.py
@@ -7,7 +7,7 @@ import transpositionDecrypt
 
 
 def main():
-    myMessage = "" # Message to be hacked
+    myMessage = "AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na  indeit n uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb  nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v  nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa  toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d ro hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu thllnrshicwsg etriebruaisss  d iorr." # Message to be hacked
     hackedMessage = hackTransposition(myMessage)
 
     if hackedMessage is None:
