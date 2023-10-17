3d6289a3e1fae338edf260dedb658ee979a30187
weisje <20269574+weisje@users.noreply.github.com>
2023-10-17 07:17:46 -0400

    -Worked with hackTransposition() function     -Note: The book wanted me to add key prompts here on how to stop the code early by printing common CTRL+(Whatever) prompts but that seemed to go against the MVP of this project.  Did not include it here.     -Began forloop to iterate through all keys up to the length of the message     -Submitted key to the transpositionDecrypt.decryptMessage() function     -Passed result of above to detectEnglish.isEnglish() function     -Displayed possible decryption to user asking for confirmation on message         -If it is a good one, then it will stop, if not it will continue iterating through keys     -Removed TODO flag

--- Exercises/transpositionHacker.py
+++ Exercises/transpositionHacker.py
@@ -10,7 +10,6 @@ def main():
     pass
 
 
-# TODO
 def hackTransposition(message) -> str:
     """
     Function to iterate through all possible keys to attempt to find decoded text from an encrypted transposition cipher
@@ -18,7 +17,18 @@ def hackTransposition(message) -> str:
     :type message: str
     :return: str
     """
-    pass
+    # Note: The book wanted me to add key prompts here on how to stop the code early but that seemed to go against the MVP of this project.  Did not include it here.
+    for key in range(1, len(message)): # Loop through all possible keys up to the length of the provided message
+        if key % 10 == 0:
+            print(f"Trying key: {key}...")
+        decryptedText = transpositionDecrypt.decryptMessage(key, message) # Submit current key to the transpositionDecrypt script to attempt to decrypt it
+        if detectEnglish.isEnglish(decryptedText): # Check to see if the current message falls into the threshold of what we have defined as English
+            print("\nPossible encryption hack:") # Tell the user there's a possible decryption
+            print(f"key {key}: {decryptedText[:100]}\n")
+            print("Enter (D)one or press any other key to continue hacking") # Prompt user if this is correct or to keep going
+            response = input("> ")
+            if response.strip().upper().startswith("D"):
+                return decryptedText
 
 
 if __name__ == '__main__':
