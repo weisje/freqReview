dc78ba3d6a47a4427529ee4ad24e0fc7cb35234f
weisje <20269574+weisje@users.noreply.github.com>
2023-10-17 07:05:37 -0400

    -Created Exercises > transpositionHacker.py file     -File will be used for exploring the book's code on how to use the detectEnglish.py code to rattle through a bunch of different transposition cipher keys -Added header comments -Added import statements -Generated framing for main() & hackTransposition() functions

---
+++ Exercises/transpositionHacker.py
@@ -0,0 +1,25 @@
+# Transposition Cipher Hacker
+# https://www.nostarch.com/crackingcodes (BSC Licensed)
+
+import detectEnglish
+import pyperclip
+import transpositionDecrypt
+
+
+def main():
+    pass
+
+
+# TODO
+def hackTransposition(message) -> str:
+    """
+    Function to iterate through all possible keys to attempt to find decoded text from an encrypted transposition cipher
+    :param message: Encrypted transposition text to be decoded
+    :type message: str
+    :return: str
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
