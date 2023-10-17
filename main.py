c6f28e2749311a2cc5ba275ce0968372e9787c62
weisje <20269574+weisje@users.noreply.github.com>
2023-10-17 07:54:56 -0400

    Tested new function functionality     -Copied sample message from website & pasted it into myMessage     -Result: "key 6: Augusta Ada King-Noel, Countess of Lovelace (10 December 1815 - 27 November 1852) was an English mat"     -Tried again, not stopping it when it came across key 6     -Result: "Trying key: 400...                 Trying key: 410...                 Failed to hack message"

--- main.py
+++ main.py
@@ -424,7 +424,8 @@ def sarcasmCipher(sourceText):
 
 
 def main():
-    print(isEnglish("..... Hat !!!!! Ball ????? ~~~~~~~~~~~~~~~~~~~~ ******** (((((((((( 9098765432"))
+    myMessage = "AaKoosoeDe5 b5sn ma reno ora'lhlrrceey e  enlh na  indeit n uhoretrm au ieu v er Ne2 gmanw,forwnlbsya apor tE.no euarisfatt  e mealefedhsppmgAnlnoe(c -or)alat r lw o eb  nglom,Ain one dtes ilhetcdba. t tg eturmudg,tfl1e1 v  nitiaicynhrCsaemie-sp ncgHt nie cetrgmnoa yc r,ieaa  toesa- e a0m82e1w shcnth  ekh gaecnpeutaaieetgn iodhso d ro hAe snrsfcegrt NCsLc b17m8aEheideikfr aBercaeu thllnrshicwsg etriebruaisss  d iorr."
+    hackTranspositionController(myMessage)
 
 
 if __name__ == '__main__':
