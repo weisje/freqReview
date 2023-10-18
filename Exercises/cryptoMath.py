75e3143f6a3a3692d943a56f143324b09f29a79f
weisje <20269574+weisje@users.noreply.github.com>
2023-10-18 07:15:07 -0400

    Created functions gcd() & findModInverse() & their intial comments within cryptoMath.py

---
+++ Exercises/cryptoMath.py
@@ -0,0 +1,28 @@
+# Cryptomath Module
+# https://www.nostarch.com/crackingcodes (BSD Licensed)
+
+
+# TODO
+def gcd(a, b) -> int:
+    """
+    Function for finding the greatest common denominator of two provided integers
+    :param a: First value to compare
+    :type a: int
+    :param b: Second value to compare
+    :type b: int
+    :return: int
+    """
+    pass
+
+
+# TODO
+def findModInverse(a, m) -> int:
+    """
+    Function for finding the modulo inverse of two provided integers
+    :param a: First value to calculate
+    :type a: int
+    :param m: Second value to calculate
+    :type m: int
+    :return: int
+    """
+    pass
