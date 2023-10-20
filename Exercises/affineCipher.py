caa11b25131a8e44e956d61a8024908a301b4976
weisje <20269574+weisje@users.noreply.github.com>
2023-10-20 07:31:10 -0400

    -Added framework of all functions -Working with getRandomKey() function:     -Filled out initial comments     -Started while loop & generated two random integers up to the length of the SYMBOLS variable     -Checked to see if the first key("keyA") is relatively prime with the length of the SYMBOLS variable         -If so then returns keyA * len(SYMBOLS) + keyB     -Removed TODO flag

--- Exercises/affineCipher.py
+++ Exercises/affineCipher.py
@@ -9,3 +9,74 @@ import sys
 
 SYMBOLS = string.ascii_uppercase + string.ascii_lowercase + "1234567890 !?."
 
+
+def main():
+    pass
+
+
+# TODO
+def getKeyParts(key) -> tuple:
+    """
+
+    :param key:
+    :type key:
+    :return: tuple
+    """
+    pass
+
+
+# TODO
+def checkKeys(keyA, keyB, mode) -> None:
+    """
+
+    :param keyA:
+    :type keyA:
+    :param keyB:
+    :type keyB:
+    :param mode:
+    :type mode:
+    :return: None
+    """
+    pass
+
+
+# TODO
+def encryptMessage(key, message) -> str:
+    """
+
+    :param key:
+    :type key:
+    :param message:
+    :type message:
+    :return: str
+    """
+    pass
+
+
+# TODO
+def decryptMessage(key, message) -> str:
+    """
+
+    :param key:
+    :type key:
+    :param message:
+    :type message:
+    :return: str
+    """
+    pass
+
+
+def getRandomKey() -> int:
+    """
+    Function for generating two random set of integers based on the length of the SYMBOLS variable, checking to see if the first is relatively prime with the length of the SYMBOLS variable, & returning the product of the first & the length of SYMBOLS summed with the second.
+    :return: int
+    """
+    while True:
+        keyA = random.randint(2, len(SYMBOLS))
+        keyB = random.randint(2, len(SYMBOLS))
+        if cryptoMath.gcd(keyA, len(SYMBOLS)) == 1:
+            return keyA * len(SYMBOLS) + keyB
+
+
+if __name__ == '__main__':
+    main()
