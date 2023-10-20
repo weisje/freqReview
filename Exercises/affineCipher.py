b408efba69ee68bd6327e2cd27938cbc7172353c
weisje <20269574+weisje@users.noreply.github.com>
2023-10-20 07:11:31 -0400

    -Added required imports & string -Set value SYMBOL to match upper/lowercase letters, numbers, & special symbols " ?!."

---
+++ Exercises/affineCipher.py
@@ -0,0 +1,11 @@
+# Affine Cipher
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import cryptoMath
+import pyperclip
+import random
+import string
+import sys
+
+SYMBOLS = string.ascii_uppercase + string.ascii_lowercase + "1234567890 !?."
+
