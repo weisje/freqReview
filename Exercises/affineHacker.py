8990ab5733c0d3cfac80917be79f2381e36b1823
weisje <20269574+weisje@users.noreply.github.com>
2023-10-25 07:18:52 -0400

    -Added initial comments for function     -Imported required packages     -Working with hackAffine() function:         -Iterated through all keys in the range up to the square of the count of available characters         -Checked for key validity on each key with cryptoMath & checkKey         -Created decrypted text with the provided key         -Checked decrypted text with detectEnglish function         -Checked with user to see if the message is english & if not keep going         -Returned decryptedText         -Removed TODO flag

--- Exercises/affineHacker.py
+++ Exercises/affineHacker.py
@@ -1,19 +1,45 @@
 # Affine Cipher Hacker
 # https://www.nostarch.com/crackingcodes/ (BSD Licensed)
 
+import affineCipher
+import cryptoMath
+import detectEnglish
+import pyperclip
+
+
 # TODO
 def main():
     pass
 
 
-# TODO
 def hackAffine(message) -> str | None:
     """
-
-    :param message:
-    :return:
+    Function for brute-forcing messages that are suspected to be encrypted with an Affine Cipher
+    :param message: Variable to be interrogated to see if it can be decrypted
+    :type message: str
+    :return: str
     """
-    pass
+    print("Hacking...")
+
+    for key in range(len(affineCipher.SYMBOLS) ** 2): # Iterate through all the possible keys up to the square of the length of allowed symbols
+        keyA = affineCipher.getKeyParts(key)[0]
+        if cryptoMath.gcd(keyA, len(affineCipher.SYMBOLS)) != 1:
+            continue
+        decryptedText = affineCipher.decryptMessage(key, message)
+        if not SILENT_MODE:
+            print(f"Tried key {key}... {decryptedText[:40]}")
+
+        if detectEnglish.isEnglish(decryptedText): # If we find possible English according to the dictionary
+            print("\nPossible encryption hack:")
+            print(f"Key: {key}")
+            print(f"Decrypted message: {decryptedText[:200]}\n")
+            print("Enter (D)one if correct or press Enter to continue hacking:") # Check with user to see if we want to stop here or keep trying
+            response = input("> ")
+
+            if response.strip().upper().startswith("D"):
+                return decryptedText
+
+    return None
 
 
 if __name__ == '__main__':
