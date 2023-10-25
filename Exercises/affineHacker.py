f873b6c6e21d31fa73da7c617d48729f6903933d
weisje <20269574+weisje@users.noreply.github.com>
2023-10-25 07:25:38 -0400

    -Working with main() function:         -Populated myMessage         -Added SILENT_MODE flag         -Passed message to hackAffine             -If successful copies message to clipboard             -If not it prints that the hack was unsuccessful         -Removed TODO flag

--- Exercises/affineHacker.py
+++ Exercises/affineHacker.py
@@ -6,10 +6,20 @@ import cryptoMath
 import detectEnglish
 import pyperclip
 
+SILENT_MODE = False
 
-# TODO
+
 def main():
-    pass
+    myMessage = """"5QG9ol3La6QI93!xQxaia6faQL9QdaQG1!!axQARLa!!AuaRLQADQALQG93!xQxaGaAfaQ1QX3o1RQARL9Qda!AafARuQLX1LQALQI1iQX3o1RN"Q-5!1RQP36ARu"""
+
+    hackedMessage = hackAffine(myMessage)
+
+    if hackedMessage is not None:
+        print("Copying hacked message to clipboard: ")
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+    else:
+        print("Failed to hack encryption.")
 
 
 def hackAffine(message) -> str | None:
@@ -23,7 +33,7 @@ def hackAffine(message) -> str | None:
 
     for key in range(len(affineCipher.SYMBOLS) ** 2): # Iterate through all the possible keys up to the square of the length of allowed symbols
         keyA = affineCipher.getKeyParts(key)[0]
-        if cryptoMath.gcd(keyA, len(affineCipher.SYMBOLS)) != 1:
+        if cryptoMath.gcd(keyA, len(affineCipher.SYMBOLS)) != 1: # Skip over keys who are not relatively prime to one another
             continue
         decryptedText = affineCipher.decryptMessage(key, message)
         if not SILENT_MODE:
