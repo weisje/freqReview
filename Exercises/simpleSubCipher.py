2e58b6a94fc93a8c38d239c4a2356f34f47fb191
weisje <20269574+weisje@users.noreply.github.com>
2023-10-29 09:19:21 -0400

    -Working with main() function:     -Took in message, key, & mode variables     -Checked to see if the key is valid     -Ran cipher on message based on key     -Printed ciphered message & copied it to the clipboard     -Removed TODO flag -Tested program with message & key from book.  Confirmed that both encrypt & decrypt ran as anticipated.

--- Exercises/simpleSubCipher.py
+++ Exercises/simpleSubCipher.py
@@ -9,9 +9,22 @@ import sys
 LETTERS = string.ascii_uppercase
 
 
-# TODO
 def main():
-    pass
+    myMessage = 'If a man is offered a fact which goes against his instincts, he will scrutinize it closely, and unless the evidence is overwhelming, he will refuse to believe it. If, on the other hand, he is offered something which affords a reason for acting in accordance to his instincts, he will accept it even on the slightest evidence. The origin of myths is explained in this way. -Bertrand Russell'
+    myKey = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
+    myMode = 'encrypt'  # Set to either 'encrypt' or 'decrypt'.
+
+    if not keyIsValid(myKey):
+        sys.exit("There is an error in the key or symbol set.")
+    if myMode == 'encrypt':
+        translated = encryptMessage(myKey, myMessage)
+    elif myMode == 'decrypt':
+        translated = decryptMessage(myKey, myMessage)
+    print(f"Using key {myKey}")
+    print(f"The {myMode}ed message is: ")
+    print(translated)
+    pyperclip.copy(translated)
+    print("\nThis message has been copied to the clipboard.")
 
 
 def keyIsValid(key) -> bool:
