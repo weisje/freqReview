b4ad6c1240db3106f379727db3f653c9e99ec1eb
weisje <20269574+weisje@users.noreply.github.com>
2023-11-01 06:58:53 -0400

    -Added & ran Exercises > Resources > makeWordPatterns.py -Took result file(wordPatterns.py) & added it to the Exercises file

---
+++ Exercises/simpleSubHacker.py
@@ -0,0 +1,16 @@
+# Caesar Cipher
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import copy
+import os
+import pyperclip
+import simpleSubCipher
+import wordPatterns
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
